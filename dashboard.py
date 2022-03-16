0bb77f3c987e8d646c9ed8f809f9d3fbb6f11ef0
Sushant Shetty <sushant6195@gmail.com>
2022-03-16 12:42:57 +0530

    Added csv reading capability

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import time 
+import csv
+from keras.models import model_from_json
 
 st.title("Earthquake Anomaly Detection using Delegated Regressors")
 
-window_size = st.selectbox('Please select Window Size', (1, 2, 3, 4))
+window_size = st.selectbox('Please select Window Size', (1, 2))
 st.write('Selected window size:', window_size)
+
+now = time.time()
+row = 0
+
+while True:
+  temp_time = time.time()
+  if abs(now - temp_time) >= 10:
+    with open("2_year_data_30_min.csv") as data_file:
+      reader = csv.reader(data_file)
+      data_to_be_loaded = [row for idx, row in enumerate(reader) if idx in row]
+  
+  json_file = open(f'model_{window_size}.json', 'r')
+  loaded_model_json = json_file.read()
+  json_file.close()
+  loaded_model = model_from_json(loaded_model_json)
+  loaded_model.load_weights(f"model_{window_size}.h5")
+  prediction = loaded_model.predict(data_to_be_loaded)
+      
+    
+  
