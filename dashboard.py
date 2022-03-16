bb1f4c93ebd1f34803c51049b18d7a539ef9eeee
Sushant Shetty <sushant6195@gmail.com>
2022-03-16 14:06:33 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,15 +10,18 @@ window_size = st.selectbox('Please select Window Size', (1, 2))
 st.write('Selected window size:', window_size)
 
 now = time.time()
-row = 0
+index = 0
 plotting_data = []
+plotting_row = []
 
 while True:
   temp_time = time.time()
   if abs(now - temp_time) >= 10:
+    plotting_row.append(index)
     with open("2_year_data_30_min.csv") as data_file:
       reader = csv.reader(data_file)
-      data_to_be_loaded = [row for idx, row in enumerate(reader) if idx in row]
+      data_to_be_loaded = [data_row for idx, data_row in enumerate(reader) if idx == index]
+    index += 1
   
   json_file = open(f'model_{window_size}.json', 'r')
   loaded_model_json = json_file.read()
@@ -27,7 +30,8 @@ while True:
   loaded_model.load_weights(f"model_{window_size}.h5")
   prediction = loaded_model.predict(data_to_be_loaded)
   plotting_data.append(prediction)
-  pl
+  graph(plotting_data, plotting_row)
+  
   
       
     
