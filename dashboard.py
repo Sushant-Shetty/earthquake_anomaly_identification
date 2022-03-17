38a6a4e39178a5f841019cd7a26451b0a15b9bce
Sushant Shetty <sushant6195@gmail.com>
2022-03-17 15:42:38 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -40,7 +40,7 @@ if st.button('Fetch data'):
   
   
 st.subheader("Predicted radon values")
-st.write('Selected window size:', prediction.shape)
+st.write('Selected window size:', prediction)
 st.write('Selected window size:', type(st.session_state.plotting_row))
 
 fig, ax = plt.subplots(figsize = (30, 12))
