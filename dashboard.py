dc5bdc7b0d44ba6d3152ed5a3e3f44e0c2ce4991
Sushant Shetty <sushant6195@gmail.com>
2022-03-17 13:10:37 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -39,7 +39,7 @@ if st.button('Fetch data'):
   
   
 st.subheader("Predicted radon values")
-st.write('Selected window size:', index)
+st.write('Selected window size:', st.session_state.index)
 st.write('Selected window size:', plotting_row)
 # st.line_chart(pd.DataFrame(pd.DataFrame(plotting_data), pd.DataFrame(plotting_row)))
   
