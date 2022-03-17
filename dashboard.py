df2106eb8eb60d4fc4babeb6be4f74c70c470902
Sushant Shetty <sushant6195@gmail.com>
2022-03-17 16:15:30 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -34,13 +34,13 @@ if st.button('Fetch data'):
   json_file.close()
   loaded_model = model_from_json(loaded_model_json)
   loaded_model.load_weights(f"model_{window_size}.h5")
-  prediction = loaded_model.predict(data_to_be_loaded).reshape(1,1)
+  prediction = loaded_model.predict(data_to_be_loaded)
   prediction = prediction.tolist()
   st.session_state.plotting_data.append(prediction)
   
   
 st.subheader("Predicted radon values")
-st.write('Selected window size:', st.session_state.plotting_data)
+st.write('Selected window size:', st.session_state.plotting_data.shape)
 st.write('Selected window size:', st.session_state.plotting_row)
 
 fig, ax = plt.subplots(figsize = (30, 12))
